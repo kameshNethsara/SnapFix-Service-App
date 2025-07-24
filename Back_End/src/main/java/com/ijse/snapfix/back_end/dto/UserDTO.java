c68f90787366049e36047b4f6b8aa4da49b31cda
kameshNethsara <kamesh317nethsara@gmail.com>
2025-07-24 09:41:49 +0530

    Add UserDTO class to encapsulate user details and security information

---
+++ Back_End/src/main/java/com/ijse/snapfix/back_end/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.ijse.snapfix.back_end.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+    private int userId;
+    ///////////User Details//////////////
+    private String userFullName;
+    private String userAddress;
+    private String userEmail;
+    private String userMobile;
+    private String userRole;
+    private String userDepartment;
+    private String userInfo;
+    private LocalDate userWhenCreated; //date
+    ///////////User Security Details/////////////
+    private String userName;
+    private String userPassword;
+}
